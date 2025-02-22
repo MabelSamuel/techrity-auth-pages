ab5ec9c2e8cba355760d8d2539a2f19c01e4cff2
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-02-22 22:38:43 +0100

    formatted component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,31 @@ export default function Home() {
   return (
     <div className="pl-[218px] pt-[240px] pb-[223px] pr-[352px] h-full">
       <div className="mx-[35.5px] mb-[35px] space-y-[40px]">
-        <Title content="Sign Up to Techrity"/>
-        <div className="">
-          <label htmlFor="" className="font-adamina text-[20px] text-neutral">Continue with email</label>
-          <input type="email" />
+        <Title content="Sign Up to Techrity" />
+        <div className="space-y-[16px]">
+          <label
+            htmlFor=""
+            className="font-adamina text-[20px] text-neutral block"
+          >
+            Continue with email
+          </label>
+          <input
+            type="email"
+            className="w-full px-[24px] py-[16px] border border-secondary100 rounded-[8px]"
+            placeholder="example@gmail.com"
+          />
         </div>
       </div>
       <div className="space-y-[32px] text-secondary100 ">
-        <p className="text-center">By creating an accounting you agree with our Terms of Services, Privacy Policy, and our default Newsletters. </p>
+        <p className="text-center">
+          By creating an accounting you agree with our Terms of Services,
+          Privacy Policy, and our default Newsletters.{" "}
+        </p>
         <div className="flex space-x-[8px] text-[20px] justify-center">
           <span>Already have an account ?</span>
-          <Link href="/login" className="font-medium underline text-black" >Login</Link>
+          <Link href="/login" className="font-medium underline text-black">
+            Login
+          </Link>
         </div>
       </div>
     </div>
